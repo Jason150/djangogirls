09a522af5b447d894c547a889c8f064c5ce48657
Jason150 <31621587+Jason150@users.noreply.github.com>
2018-10-06 16:27:46 +0900

    authentication
    edit/delete

--- blog/views.py
+++ blog/views.py
@@ -1,14 +1,15 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Post
 from django.utils import timezone
+from .forms import PostForm
 
 # Create your views here.
 def home(request):
-    return render(request, 'home.html')
+    return redirect(post_list)
 
 def post_list(request):
     posts = Post.objects.filter()\
-        .order_by('published_date')
+        .order_by('-published_date')
     return render(
         request,
         'post_list.html',
@@ -22,5 +23,36 @@ def post_detail(request, pk):
         'post_detail.html',
         {'content': post}
     )
+
+def post_new(request):
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm()
+    return render(
+        request,
+        'post_edit.html',
+        {'form': form}
+    )
+
+def post_edit(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm(instance=post)
+    return render(request, 'post_edit.html', {'form':form})
 
     
