08b227e77b03ef74fa1b5e6464b91d56eeecd62a
Jason150 <31621587+Jason150@users.noreply.github.com>
2018-10-06 14:03:55 +0900

    add template
    post_list template

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,18 @@
 from django.shortcuts import render
+from .models import Post
+from django.utils import timezone
 
 # Create your views here.
 def home(request):
     return render(request, 'home.html')
 
 def post_list(request):
-    return render(request, 'post_list.html')
+    posts = Post.objects.filter()\
+        .order_by('published_date')
+    return render(
+        request,
+        'post_list.html',
+        {'contents': posts}
+    )
+
+    
